fc413254f7ad2c2c2c4afe7b729db7089c3f480a
GitHub Action <action@github.com>
2022-07-09 22:39:28 +0000

    update data

--- docs/_next/static/aaS5HXo72ZkmBKLf0F3kz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6e53deb1925f6dff.css","static/chunks/pages/index-92980b129b7dd026.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
